550b5aaf30ced7e126e43f19b8078aba12c96aa9
Danielle <danimeye@umich.edu>
2018-09-29 21:59:59 -0400

    finished hw2

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -11,10 +11,12 @@
 #############################
 ##### IMPORT STATEMENTS #####
 #############################
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
+import requests
+import json 
 
 #####################
 ##### APP SETUP #####
@@ -27,8 +29,70 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 ###### FORMS #######
 ####################
 
+### Part 1 ###
+
+@app.route('/artistform')
+def artistForm():
+	return render_template('artistform.html')
 
 
+@app.route('/artistinfo')
+def artistInfo():
+	if request.method == 'GET':
+		artist = request.args.get('artist')
+		base_url = "https://itunes.apple.com/search"
+		params_diction = {}
+		params_diction["term"] = artist
+		params_diction["country"] = 'US'
+		resp = requests.get(base_url, params = params_diction)
+		text = resp.text
+		python_obj = json.loads(text)
+		result_obj = python_obj["results"]
+	return render_template('artist_info.html', objects = result_obj)
+
+
+@app.route('/specific/song/<artist_name>')
+def specific(artist_name):
+	if request.method == 'GET':
+		base_url = "https://itunes.apple.com/search"
+		params_diction = {}
+		params_diction["term"] = artist_name
+		params_diction["country"] = 'US'
+		resp = requests.get(base_url, params = params_diction)
+		text = resp.text
+		python_obj = json.loads(text)
+		result_obj = python_obj["results"]
+
+	return render_template('specific_artist.html', results = result_obj)
+
+
+@app.route('/artistlinks')
+def artistLinks():
+	return render_template('artist_links.html')
+
+### Part 2 ###
+
+class AlbumEntryForm(FlaskForm):
+	album_name = StringField('Enter the name of an album:', validators = [Required()])
+	rating = RadioField('How much do you like this album? (1 low, 3 high)', choices = [('1', '1'), ('2', '2'), ('3', '3')], validators = [Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/album_entry')
+def albumEntry():
+	this_form = AlbumEntryForm()
+	return render_template('album_entry.html', form = this_form)
+
+@app.route('/album_result', methods = ['GET', 'POST'])
+def albumResults():
+	this_form = AlbumEntryForm()
+	print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	if this_form.validate_on_submit():
+		print("**********************************")
+		album_title = this_form.album_name.data
+		user_rating = this_form.rating.data
+		return render_template('album_data.html', album_name = album_title, rating = user_rating)
+	flash('All fields are required!')
+	return redirect(url_for('albumEntry'))
 
 ####################
 ###### ROUTES ######
